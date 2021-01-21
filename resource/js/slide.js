4d6d8f0e947d1fc468e51062665a77c4fe1489c6
TestProject-Learn-Zone <76813875+TestProject-Learn-Zone@users.noreply.github.com>
2021-01-21 14:38:29 +0630

    Add files via upload

---
+++ resource/js/slide.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
